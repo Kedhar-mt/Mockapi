81f26d2d10f7215b80d9d8882789d46cb480a7d5
kedhareswar Matha <kedharmatha999@gmail.com>
2025-04-04 16:36:18 +0530

    hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
+    strictPort: true,
+    allowedHosts: ['https://mockapi-frontend.onrender.com/'], // Add your Render frontend URL here
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    strictPort: true
   }
 });
